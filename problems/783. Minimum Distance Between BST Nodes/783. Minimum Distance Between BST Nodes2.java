de938b864cd14f6f67797d29d413172d4fe4adf5
cdepanagoudra <cdepanagoudra@gmail.com>
2023-02-18 00:06:45 +0530

    format corrected

--- problems/783. Minimum Distance Between BST Nodes/783. Minimum Distance Between BST Nodes2.java
+++ problems/783. Minimum Distance Between BST Nodes/783. Minimum Distance Between BST Nodes2.java
@@ -1,27 +1,27 @@
 class Solution {
-  int minDistance = Integer.MAX_VALUE;
-  // Initially, it will be null.
-  TreeNode prevValue;
+	int minDistance = Integer.MAX_VALUE;
+	// Initially, it will be null.
+	TreeNode prevValue;
 
-  void inorderTraversal(TreeNode root) {
-    if (root == null) {
-      return;
-    }
+	void inorderTraversal(TreeNode root) {
+		if (root == null) {
+			return;
+		}
 
-    inorderTraversal(root.left);
+		inorderTraversal(root.left);
 
-    // Find the difference with the previous value if it is there.
-    if (prevValue != null) {
-      minDistance = Math.min(minDistance, root.val - prevValue.val);
-    }
-    prevValue = root;
+		// Find the difference with the previous value if it is there.
+		if (prevValue != null) {
+			minDistance = Math.min(minDistance, root.val - prevValue.val);
+		}
+		prevValue = root;
 
-    inorderTraversal(root.right);
-  }
+		inorderTraversal(root.right);
+	}
 
-  public int minDiffInBST(TreeNode root) {
-    inorderTraversal(root);
+	public int minDiffInBST(TreeNode root) {
+		inorderTraversal(root);
 
-    return minDistance;
-  }
+		return minDistance;
+	}
 };
