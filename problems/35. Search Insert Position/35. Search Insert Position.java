bc8dfc7def5aa859ee89e3c1577a31adce02272f
cdepanagoudra <cdepanagoudra@gmail.com>
2023-02-20 23:04:52 +0530

    35. Search Insert Position

--- problems/35. Search Insert Position/35. Search Insert Position.java
+++ problems/35. Search Insert Position/35. Search Insert Position.java
@@ -1,10 +1,12 @@
 class Solution {
     public int searchInsert(int[] nums, int target) {
         int start = 0;
+        
         int end = nums.length - 1;
 
         while (start <= end) {
             int mid = start + (end-start)/2;
+            // mid = start + (end-start)/2 , here start + because we will get a min value and that we have to add with start , we will get correct mid
             if (nums[mid] == target) return mid;
             else if (nums[mid] > target) end = mid-1;
             else start = mid + 1;
