a4ff70f7387a3843f4af152dcd6ff9c59e667fd3
cdepanagoudra <cdepanagoudra@gmail.com>
2024-12-03 23:04:02 +0530

    Isomorphic Strings

---
+++ String/Isomorphic Strings.java
@@ -0,0 +1,28 @@
+
+class Solution {
+    public boolean isIsomorphic(String s, String t) {
+        if (s.length() != t.length()) {
+            return false;
+        }
+
+        int[] lastSeenS = new int[256];
+        int[] lastSeenT = new int[256];
+
+        for (int i = 0; i < s.length(); i++) {
+            char charS = s.charAt(i);
+            char charT = t.charAt(i);
+
+            // Check if the last seen positions differ
+            if (lastSeenS[charS] != lastSeenT[charT]) {
+                return false;
+            }
+
+            // Update the last seen positions
+            lastSeenS[charS] = i + 1;
+            lastSeenT[charT] = i + 1;
+        }
+
+        return true;
+    }
+}
+
