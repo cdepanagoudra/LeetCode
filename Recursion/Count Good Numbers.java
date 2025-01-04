4465b0142c0d61c41015b55a9f4150c2105c838f
cdepanagoudra <cdepanagoudra@gmail.com>
2025-01-04 22:23:03 +0530

    added count good numbers

---
+++ Recursion/Count Good Numbers.java
@@ -0,0 +1,24 @@
+class Solution {
+    long p = 1000000007;
+    public long power(long x,long y){
+        long res = 1;
+        if(x==0){
+            return 0;
+        }
+        while(y>0){
+            if((y&1)==1){
+                res = (res*x)%p;
+            }
+            y = y>>1;
+            x = (x*x)%p;
+        }
+        return  res;
+    }
+    public int countGoodNumbers(long n) {
+        long count_4 = n/2;
+        long count_5 = n - count_4;
+        long ans = (((power(4, count_4) % p)  * (power(5, count_5) % p) % p));
+       
+        return (int) ans;
+    }
+}
